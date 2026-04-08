b0abf58fa35e2d02dceb98b1ea39de6170464d8d
seedy <seedy.dupuis@gmail.com>
2026-04-08 11:53:29 +0200

    chore(app): setup bucket and endpoints

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  distDir: ".next",
+  
 };
 
 export default nextConfig;
